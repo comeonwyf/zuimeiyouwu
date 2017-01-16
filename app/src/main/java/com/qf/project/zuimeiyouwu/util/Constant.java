a2b48b80f9695ce4819480474af9dab3682153c6
comeonwyf <wyf19881116>
2017-01-16 17:29:48 +0800

    增加一个画报实体类，和url常量接口

---
+++ app/src/main/java/com/qf/project/zuimeiyouwu/util/Constant.java
@@ -0,0 +1,8 @@
+package com.qf.project.zuimeiyouwu.util;
+
+/**
+ * Created by Administrator on 2017/1/16.
+ */
+public interface Constant {
+    String HUABAO_URL = "http://design.zuimeia.com/api/v1/articles/daily/simple/?page=1&is_new_user=false&page_size=30&user_id=0&device_id=861423034616922&platform=android&lang=zh&appVersion=1.2.6&appVersionCode=10260&systemVersion=23&countryCode=CN&user_id=0&token=&package_name=com.zuiapps.zuiworld";
+}
