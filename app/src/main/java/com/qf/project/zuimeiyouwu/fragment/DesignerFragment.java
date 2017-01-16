5b3afacf3ced9fd489a6320b083604e835d364c2
luojianping168 <178999895@qq.com>
2017-01-16 16:18:47 +0800

    罗健平_添加了个frament

---
+++ app/src/main/java/com/qf/project/zuimeiyouwu/fragment/DesignerFragment.java
@@ -0,0 +1,14 @@
+package com.qf.project.zuimeiyouwu.fragment;
+
+import com.qf.chenhao.mr_chenlibrary.base.BaseFragment;
+import com.qf.project.zuimeiyouwu.R;
+
+/**
+ * Created by Administrator on 2017/1/16.
+ */
+public class DesignerFragment extends BaseFragment{
+    @Override
+    protected int getContentId() {
+        return R.layout.designer_fragment_layout;
+    }
+}
