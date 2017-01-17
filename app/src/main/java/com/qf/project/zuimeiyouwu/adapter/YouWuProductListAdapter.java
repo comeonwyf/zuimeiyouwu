34517febafb316dc394d8429bab766072245011a
comeonwyf <wu_yu_feng@hotmail.com>
2017-01-17 16:54:32 +0800

    添加了导航优化_wuyufeng

---
+++ app/src/main/java/com/qf/project/zuimeiyouwu/adapter/YouWuProductListAdapter.java
@@ -0,0 +1,30 @@
+package com.qf.project.zuimeiyouwu.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Administrator on 2017/1/17.
+ */
+public class YouWuProductListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
--- app/src/main/java/com/qf/project/zuimeiyouwu/fragment/YouWuViewPagerfragment.java
+++ app/src/main/java/com/qf/project/zuimeiyouwu/fragment/YouWuViewPagerfragment.java
@@ -1,21 +1,29 @@
 package com.qf.project.zuimeiyouwu.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.widget.TextView;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import com.qf.chenhao.mr_chenlibrary.base.BaseFragment;
 import com.qf.project.zuimeiyouwu.Entity.TabEntity;
 import com.qf.project.zuimeiyouwu.R;
+import com.qf.project.zuimeiyouwu.adapter.YouWuProductListAdapter;
+import com.qf.project.zuimeiyouwu.util.Constant;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
-import butterknife.Bind;
+import okhttp3.Call;
+
 
 /**
  * Created by Administrator on 2017/1/16.
  */
 public class YouWuViewPagerfragment extends BaseFragment {
-    @Bind(R.id.text)
-    public TextView text;
+    private RelativeLayout subtitlesContainer;
+    private ListView listView;
+
     @Override
     protected int getContentId() {
         return R.layout.fragment_youwu_viewpager_layout;
@@ -33,13 +41,35 @@ public class YouWuViewPagerfragment extends BaseFragment {
     }
 
     @Override
-    protected void loadDatas() {
-
+    protected void init(View view) {
+        subtitlesContainer = findViewByIds(view,R.id.subtitle_container);//子标题的容器
+        listView = findViewByIds(view, R.id.listview);
     }
 
     @Override
     protected void getDatas(Bundle bundle) {
         TabEntity entity = (TabEntity) bundle.getSerializable("datas");
-        text.setText(entity.getName());
+
+        if(entity.getName().equals("Daily")){//daily界面
+
+        }else {//其他界面
+            if(entity.getName().equals("Men")){
+                subtitlesContainer.setVisibility(View.GONE);
+            }
+            String url = Constant.TAB_DETAILS_BEFORE_URL+entity.getId()+Constant.TAB_DETAILS_END_URL;
+            OkHttpUtils.get().url(url).build().execute(new StringCallback() {
+                @Override
+                public void onError(Call call, Exception e, int id) {
+                }
+                @Override
+                public void onResponse(String response, int id) {
+                    //获得产品列表
+                    Log.e("print", "onResponse: "+response );
+                }
+            });
+            YouWuProductListAdapter adapter = new YouWuProductListAdapter();
+            listView.setAdapter(adapter);
+        }
+
     }
 }
