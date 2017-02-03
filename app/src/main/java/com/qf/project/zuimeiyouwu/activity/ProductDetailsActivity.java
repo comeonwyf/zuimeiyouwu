45b6a32f90b784463dd0d641d2b91b3954e57ed2
comeonwyf <wu_yu_feng@hotmail.com>
2017-02-03 14:59:16 +0800

    产品详情页_wuyufeng

--- app/src/main/java/com/qf/project/zuimeiyouwu/activity/ProductDetailsActivity.java
+++ app/src/main/java/com/qf/project/zuimeiyouwu/activity/ProductDetailsActivity.java
@@ -1,9 +1,12 @@
 package com.qf.project.zuimeiyouwu.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
@@ -11,8 +14,11 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -39,6 +45,7 @@ import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.util.List;
 
+import butterknife.Bind;
 import okhttp3.Call;
 
 public class ProductDetailsActivity extends BaseActivity {
@@ -47,12 +54,16 @@ public class ProductDetailsActivity extends BaseActivity {
     private ProductListEntity.DataEntity.ProductsEntity productsEntity;
     private ViewPager rollPagerView;
     private TabView tabview;
-    private ListView listView;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+    private TextView productName;
+    public ImageView iv1;
+    private TextView tv1;
+    public ImageView iv2;
+    private TextView tv2;
+    public ImageView iv3;
+    private TextView tv3;
+    private WebView webView;
+    private TextView productDescTV;
+    private LinearLayout container_ll;
 
     @Override
     protected int getContentId() {
@@ -70,13 +81,24 @@ public class ProductDetailsActivity extends BaseActivity {
         productsEntity = (ProductListEntity.DataEntity.ProductsEntity) intent.getSerializableExtra("datas");
         rollPagerView = findViewByIds(R.id.rollpagerview);
         tabview = findViewByIds(R.id.tabview);
-        listView = findViewByIds(R.id.listview);
+        productName = findViewByIds(R.id.name);
+        iv1 = findViewByIds(R.id.iv1);
+        tv1 = findViewByIds(R.id.tv1);
+        iv2 = findViewByIds(R.id.iv2);
+        tv2 = findViewByIds(R.id.tv2);
+        iv3 = findViewByIds(R.id.iv3);
+        tv3 = findViewByIds(R.id.tv3);
+        productDescTV = findViewByIds(R.id.productDescTV);
+        container_ll = findViewByIds(R.id.container_ll);
+        webView = findViewByIds(R.id.webview_id);
+
     }
 
     @Override
     protected void loadDatas() {
         //获得下载产品详情实体类URL
         String url = String.format(Constant.PRODUCT_DETAILS_URL, productsEntity.getId());
+        Log.e(TAG, "loadDatas: "+url);
         OkHttpUtils.get().url(url).build().execute(new StringCallback() {
             @Override
             public void onError(Call call, Exception e, int id) {
@@ -88,40 +110,83 @@ public class ProductDetailsActivity extends BaseActivity {
                 ProductDetailsEntity productDetailsEntity = JsonUtil.getProductDeatailsByJSON(response);
                 //设置广告轮播页
                 setBanner(productDetailsEntity.getData().getCover_images());
+                //设置产品名称
+                AssetManager assets = getAssets();
+                Typeface fromAsset = Typeface.createFromAsset(assets, "fonts/FZLanTingHeiS_Bold.otf");
+                productName.setTypeface(fromAsset);
+                productName.setText(productDetailsEntity.getData().getName());
+
                 //设置产品功能数据
-                setProductDesc(productDetailsEntity.getData().getDesc_types());
+                if(productDetailsEntity.getData().getDesc_types().size()!=0){
+                    container_ll.setVisibility(View.VISIBLE);
+                    setProductDesc(productDetailsEntity.getData().getDesc_types());
+                }else {
+                    productDescTV.setVisibility(View.VISIBLE);
+                    productDescTV.setText(productDetailsEntity.getData().getDigest());
+                }
+
+                //设置产品材质
+                setWebview(productDetailsEntity.getData().getDetail_url());
             }
         });
     }
 
+    private void setWebview(String detail_url) {
+        //返回适合手机端浏览的页面：支持JS
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        //默认情况下：webView加载的页面会用默认的浏览器打开，如果需要用自己的应用打开，需要设置WebClient
+        webView.setWebViewClient(new WebViewClient(){
+            private ProgressDialog dialog;
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                if(dialog == null){
+                    dialog =  new ProgressDialog(ProductDetailsActivity.this);
+                    dialog.setTitle("正在加载数据。。。");
+                    dialog.setMessage("请稍等");
+                    dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    dialog.show();
+                }
+
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                if (dialog.isShowing()){
+                    dialog.dismiss();
+                }
+            }
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl(detail_url);
+    }
+
     //设置产品功能数据
     private void setProductDesc(List<ProductDetailsEntity.DataEntity.DescTypesEntity> desc_types) {
+        int width = getResources().getDisplayMetrics().widthPixels/15;
+        //产品描述1
+        iv1.setMaxWidth(width);
+        iv1.setScaleType(ImageView.ScaleType.FIT_XY);
+        Glide.with(this).load(desc_types.get(0).getImage_url()).into(iv1);
+        tv1.setText(desc_types.get(0).getDesc());
+        //产品描述2
+        iv2.setMaxWidth(width);
+        iv2.setScaleType(ImageView.ScaleType.FIT_XY);
+        Glide.with(this).load(desc_types.get(1).getImage_url()).into(iv2);
+        tv2.setText(desc_types.get(1).getDesc());
+        //产品描述3
+        iv3.setMaxWidth(width);
+        iv3.setScaleType(ImageView.ScaleType.FIT_XY);
+        Glide.with(this).load(desc_types.get(2).getImage_url()).into(iv3);
+        tv3.setText(desc_types.get(2).getDesc());
 
-        MyBaseAdapter adapter = new MyBaseAdapter(this, desc_types);
-        listView.setAdapter(adapter);
-        setListViewHeightBasedOnChildren(listView);
     }
 
-    private void setListViewHeightBasedOnChildren(ListView listView) {
-
-        ListAdapter listAdapter = listView.getAdapter();
-        if (listAdapter == null) {
-            return;
-        }
-
-        int totalHeight = 0;
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            Log.e(TAG, "setListViewHeightBasedOnChildren: "+listAdapter.getCount());
-            View listItem = listAdapter.getView(i, null, listView);
-            listItem.measure(0, 0);
-            totalHeight += listItem.getMeasuredHeight();
-        }
-
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = totalHeight
-                + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
-        listView.setLayoutParams(params);
-    }
 
     //设置广告轮播页
     private void setBanner(final List<String> cover_images) {
@@ -147,56 +212,5 @@ public class ProductDetailsActivity extends BaseActivity {
 
 
     }
-    private class MyBaseAdapter extends BaseAdapter {
-        private Activity activity;
-        private List<ProductDetailsEntity.DataEntity.DescTypesEntity> desc_types;
 
-        public MyBaseAdapter(Activity activity, List<ProductDetailsEntity.DataEntity.DescTypesEntity> desc_types) {
-            this.activity = activity;
-            this.desc_types = desc_types;
-        }
-
-        @Override
-        public int getCount() {
-            return desc_types.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return desc_types.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder viewHolder;
-            if (convertView == null) {
-                viewHolder = new ViewHolder();
-                convertView = getLayoutInflater().inflate(R.layout.item_youwu_productdetails_listview, parent,false);
-                viewHolder.imageView = (ImageView) convertView.findViewById(R.id.iv);
-                ViewGroup.LayoutParams layoutParams = viewHolder.imageView.getLayoutParams();
-                layoutParams.height = ScreenUtil.getScreenWidth(ProductDetailsActivity.this)/18;
-                layoutParams.width = layoutParams.height;
-                viewHolder.imageView.setLayoutParams(layoutParams);
-                viewHolder.textView = (TextView) convertView.findViewById(R.id.tv);
-                convertView.setTag(viewHolder);
-            } else {
-                viewHolder = (ViewHolder) convertView.getTag();
-            }
-            viewHolder.textView.setText(desc_types.get(position).getDesc());
-            ImageView imageView = viewHolder.imageView;
-            Glide.with(activity).load(desc_types.get(position).getImage_url())
-                    .into(imageView);
-            return convertView;
-        }
-
-        class ViewHolder {
-            ImageView imageView;
-            TextView textView;
-        }
-    }
 }
